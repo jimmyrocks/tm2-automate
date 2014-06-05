b40aab52c5d8b06b06cc81e9aec98d0c37b538ea
Jim McAndrew <jim@loc8.us>
2014-06-05 14:39:43 -0600

    Add TMS commands

--- src/tilemath.js
+++ src/tilemath.js
@@ -1,4 +1,18 @@
 module.exports = {
+  //http://alastaira.wordpress.com/2011/07/06/converting-tms-tile-coordinates-to-googlebingosm-tile-coordinates/
+  tile2Tms: function(z, y, x) {
+    var yMax = 1 << z,
+      newY = yMax - y - 1;
+    return {
+      z: z,
+      y: newY,
+      x: x
+    };
+  },
+  lat2tms: function(lat, zoom) {
+    var yMax = 1 << zoom;
+    return yMax - (module.exports.lat2tile(lat, zoom)) - 1;
+  },
   //http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
   long2tile: function(lon, zoom) {
     return (Math.floor((lon + 180) / 360 * Math.pow(2, zoom)));
@@ -14,37 +28,35 @@ module.exports = {
     return (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n))));
   },
   //http://www.gal-systems.com/2011/07/convert-coordinates-between-web.html
-  toWgs84: function(mercatorX_lon, mercatorY_lat) {
-    if (Math.abs(mercatorX_lon) < 180 && Math.abs(mercatorY_lat) < 90)
+  toWgs84: function(mercatorLonX, mercatorLatY) {
+    if (Math.abs(mercatorLonX) < 180 && Math.abs(mercatorLatY) < 90)
       return;
 
-    if ((Math.abs(mercatorX_lon) > 20037508.3427892) || (Math.abs(mercatorY_lat) > 20037508.3427892))
+    if ((Math.abs(mercatorLonX) > 20037508.3427892) || (Math.abs(mercatorLatY) > 20037508.3427892))
       return;
 
-    var x = mercatorX_lon,
-      y = mercatorY_lat,
+    var x = mercatorLonX,
+      y = mercatorLatY,
       num3 = x / 6378137.0,
       num4 = num3 * 57.295779513082323,
       num5 = Math.floor((num4 + 180.0) / 360.0),
       num6 = num4 - (num5 * 360.0),
-      num7 = 1.5707963267948966 - (2.0 * Math.atan(Math.exp((-1.0 * y) / 6378137.0))),
-      mercatorX_lon_out = num6,
-      mercatorY_lat_out = num7 * 57.295779513082323;
+      num7 = 1.5707963267948966 - (2.0 * Math.atan(Math.exp((-1.0 * y) / 6378137.0)));
     return {
       'lat': num6,
       'lon': num7 * 57.295779513082323
     };
   },
-  toWebMercator: function(mercatorX_lon, mercatorY_lat) {
-    if ((Math.abs(mercatorX_lon) > 180 || Math.abs(mercatorY_lat) > 90))
+  toWebMercator: function(mercatorLonX, mercatorLatY) {
+    if ((Math.abs(mercatorLonX) > 180 || Math.abs(mercatorLatY) > 90))
       return;
 
-    var num = mercatorX_lon * 0.017453292519943295,
+    var num = mercatorLonX * 0.017453292519943295,
       x = 6378137.0 * num,
-      a = mercatorY_lat * 0.017453292519943295;
+      a = mercatorLatY * 0.017453292519943295;
     return {
       'lat': x,
       'lon': 3189068.5 * Math.log((1.0 + Math.sin(a)) / (1.0 - Math.sin(a)))
     };
   }
-}
+};
