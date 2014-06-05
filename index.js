0440721495ec13f6b32676bc3b5831ede12b1835
Jim McAndrew <jim@loc8.us>
2014-06-05 14:39:16 -0600

    Output tile numbers

--- index.js
+++ index.js
@@ -1,121 +1,64 @@
  var database = require('./src/database'),
    config = require('./config'),
-   http = require('http');
-
- //var tileMath = require('./src/tilemath');
+   tileMath = require('./src/tilemath'),
+   tilelive = require('tilelive');
+ tilelive.protocols['mbtiles:'] = require('mbtiles');
  //console.log(tileMath.toWgs84(tileMath.toWebMercator(-105,39.75).lat, tileMath.toWebMercator(-105,39.75).lon));
 
  var dbUpdate = {
    getBounds: function(callback) {
-     database.runScript('./sql/getNewBounds.sql', callback);
-   }
- },
-   exit = function(e) {
-     process.exit(e ? 1 : 0);
+     database.runScript('./sql/getBounds.sql', callback);
    },
-   runTm2 = {
-     options: {
-       headers: {
-         'user-agent': 'Linux Mozilla/5.0',
-         'Content-Type': 'application/json'
-       },
-       host: 'localhost',
-       port: 3000
-     },
-     updateDatasource: function(options, callback) {
-       runTm2.changeDatasource(options, function(e, r) {
-         if (e) {
-           callback(e, null);
-         } else {
-           runTm2.writeSource(r, callback);
-         }
-       });
-     },
-     writeSource: function(source, callback) {
-       var options = runTm2.options;
-       options.path = config.idSourcePath;
-       options.method = 'PUT';
-       var req = http.request(options, function(res) {
-         var data = '';
-         res.setEncoding('utf8');
-         res.on('error', function(e) {
-           callback(e, null);
-         });
-         res.on('data', function(chunk) {
-           data += chunk;
-         });
-         res.on('end', function() {
-           callback(null, data);
-         });
-       });
-       //console.log(JSON.stringify(source, null,2));
-       req.write(JSON.stringify(source));
-       req.end();
-     },
-     changeDatasource: function(options, callback) {
-       runTm2.getSource(function(e, r) {
-         if (e) {
-           callback(e, null);
-         } else {
-           //callback(e, JSON.parse(r).Layer[0].Datasource.extent, null, 2);
-           var source = JSON.parse(r);
-           source.Layer.map(function(layer) {
-             for (var option in options) {
-               if (layer && layer.Datasource) {
-                 layer.Datasource[option] = options[option];
-               }
-             }
-           });
-           callback(null, source);
-         }
-       });
-     },
-     getSource: function(callback) {
-       var options = runTm2.options;
-       options.path = config.idSourcePath;
-       options.method = 'GET';
-       var req = http.get(options, function(res) {
-         var data = '';
-         res.setEncoding('utf8');
-         res.on('error', function(e) {
-           callback(e, null);
-         });
-         res.on('data', function(chunk) {
-           data += chunk;
-         });
-         res.on('end', function() {
-           callback(null, data);
-         });
-       });
-       req.end();
-     },
-     getExtent: function(callback) {
-       runTm2.getSource(function(e, r) {
-         callback(e, JSON.parse(r).Layer[0].Datasource.extent, null, 2);
-       });
-     }
-   };
-
- dbUpdate.getBounds(function(e, r) {
-   var col = 'st_asgeojson',
-     newBounds = JSON.parse(r[0].result.rows[0][col]).coordinates[0],
-     bounds = {
-       'minLon': newBounds[0][1],
-       'maxLon': newBounds[1][1],
-       'minLat': newBounds[0][0],
-       'maxLat': newBounds[2][0]
-     },
-     boundStr = [bounds.minLat, bounds.minLon, bounds.maxLat, bounds.maxLon].join(',');
-   //boundStrWgs84 = [tileMath.toWgs84(bounds.minLat, bounds.minLon).lat, tileMath.toWgs84(bounds.minLat, bounds.minLon).lon, tileMath.toWgs84(bounds.maxLat, bounds.maxLon).lat, tileMath.toWgs84(bounds.maxLat, bounds.maxLon).lon].join(',');
-   //console.log(JSON.stringify(bounds, null, 2));
-   console.log(JSON.stringify(boundStr, null, 2));
-   runTm2.updateDatasource({
-     'extent': boundStr
-   }, function(e, r) {
-     console.log('x', e, r);
-     runTm2.getExtent(function(e, r) {
-       console.log('new', r);
-       exit();
+   getCoords: function(callback) {
+     dbUpdate.getBounds(function(e, r) {
+       if (e) {
+         callback(e);
+         return;
+       }
+       var col = 'st_asgeojson';
+       var coords = [];
+       for (var row in r[0].result.rows) {
+         coords.push(JSON.parse(r[0].result.rows[row][col]).coordinates[0]);
+       }
+       callback(null, coords);
      });
+   },
+   getTiles: function(callback, minTile, maxTile) {
+     var tiles = [];
+     minTile = minTile || 5;
+     maxTile = maxTile || 11;
+     dbUpdate.getCoords(function(e, r) {
+       if (e || !r) {
+         callback(e);
+         return;
+       }
+       r.map(function(coord) {
+         var bounds = {
+           'minLon': parseFloat(coord[0][0]),
+           'maxLon': parseFloat(coord[2][0]),
+           'minLat': parseFloat(coord[0][1]),
+           'maxLat': parseFloat(coord[1][1])
+         };
+         for (var zoom = minTile; zoom <= maxTile; zoom++) {
+           var minTmsX = tileMath.long2tile(bounds.minLon, zoom),
+             minTmsY = tileMath.lat2tms(bounds.minLat, zoom),
+             maxTmsX = tileMath.long2tile(bounds.maxLon, zoom),
+             maxTmsY = tileMath.lat2tms(bounds.maxLat, zoom);
+           for (var xRow = minTmsX; xRow <= maxTmsX; xRow++) {
+             for (var yRow = minTmsY; yRow <= maxTmsY; yRow++) {
+               tiles.push([zoom, xRow, yRow]);
+             }
+           }
+         }
+       });
+       callback(null, tiles);
+     });
+   }
+ };
+
+ dbUpdate.getTiles(function(e, r) {
+   r.map(function(tile) {
+     console.log(tile.join('/'));
    });
+   process.exit(e ? 1 : 0);
  });
