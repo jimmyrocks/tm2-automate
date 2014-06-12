c514d846a1dbddc157e870e8a14c5e3621736aad
Jim McAndrew <jim@loc8.us>
2014-06-11 18:14:03 -0600

    Allow the config to be passed to the database instead of reading it from the file

--- src/database.js
+++ src/database.js
@@ -1,85 +1,87 @@
-var config = require('../config'),
-  pg = require('pg'),
+var pg = require('pg'),
   fs = require('fs'),
-  Q = require('q'),
-  connect = function(callback) {
+  Q = require('q');
+
+module.exports = function(config) {
+  var connect = function(callback) {
     var connectionString = 'postgres://' +
       config.database.username + ':' +
       config.database.password + '@' +
       config.database.hostname + '/' +
       config.database.dbname;
     pg.connect(connectionString, callback);
-  };
-
-module.exports = {
-  runQuery: function(query, params, callback) {
-    connect(function(err, client, done) {
-      if (err) {
-        return console.error('error fetching client from pool', err);
-      }
-      client.query(query, params, function(err, result) {
-        done();
+  },
+  db = {
+    runQuery: function(query, params, callback) {
+      connect(function(err, client, done) {
         if (err) {
-          return console.error('error running query', err, query);
+          return console.error('error fetching client from pool', err);
         }
-        callback(err, result);
-      });
-    });
-  },
-  runScript: function(filename, callback) {
-    fs.readFile(filename, 'utf8', function(err, res) {
-      var queries = res.split(';');
-      if (err) {
-        callback(err);
-      } else {
-        module.exports.runQueryList(queries, callback);
-      }
-    });
-  },
-  runQueryList: function(queryObj, callback) {
-    var results = [],
-      queryIndex = -1,
-      next = function() {
-        queryIndex++;
-        if (queryIndex < queryObj.length) {
-          runQuery(queryIndex);
-        } else {
+        client.query(query, params, function(err, result) {
           done();
-        }
-      },
-      runQuery = function(i) {
-        var result = {
-          'query': queryObj[i]
-        };
-        module.exports.runQuery(
-          queryObj[i].query ? queryObj[i].query : queryObj[i].toString(),
-          queryObj[i].params ? queryObj[i].params : null,
-          function(e, r) {
-            result.err = e;
-            result.result = r;
-            results.push(result);
-            next();
-          });
-      },
-      done = function() {
-        callback(null, results);
-      };
-    next();
-  },
-  runQueryListAsync: function(queries, callback) {
-    qRunQuery = function(queryObj) {
-      var deferred = Q.defer();
-      module.exports.runQuery(
-        queryObj.query ? queryObj.query : queryObj.toString(),
-        queryObj.params ? queryObj.params : null,
-        function(e, r) {
-          deferred.resolve(e, r);
+          if (err) {
+            return console.error('error running query', err, query);
+          }
+          callback(err, result);
         });
-      return deferred.promise;
-    };
-    Q.all(queries.map(qRunQuery))
-      .done(function(e, r) {
-        callback(e, r);
       });
-  }
+    },
+    runScript: function(filename, callback) {
+      fs.readFile(filename, 'utf8', function(err, res) {
+        var queries = res.split(';');
+        if (err) {
+          callback(err);
+        } else {
+          db.runQueryList(queries, callback);
+        }
+      });
+    },
+    runQueryList: function(queryObj, callback) {
+      var results = [],
+        queryIndex = -1,
+        next = function() {
+          queryIndex++;
+          if (queryIndex < queryObj.length) {
+            runQuery(queryIndex);
+          } else {
+            done();
+          }
+        },
+        runQuery = function(i) {
+          var result = {
+            'query': queryObj[i]
+          };
+          db.runQuery(
+            queryObj[i].query ? queryObj[i].query : queryObj[i].toString(),
+            queryObj[i].params ? queryObj[i].params : null,
+            function(e, r) {
+              result.err = e;
+              result.result = r;
+              results.push(result);
+              next();
+            });
+        },
+        done = function() {
+          callback(null, results);
+        };
+      next();
+    },
+    runQueryListAsync: function(queries, callback) {
+      var qRunQuery = function(queryObj) {
+        var deferred = Q.defer();
+        db.runQuery(
+          queryObj.query ? queryObj.query : queryObj.toString(),
+          queryObj.params ? queryObj.params : null,
+          function(e, r) {
+            deferred.resolve(e, r);
+          });
+        return deferred.promise;
+      };
+      Q.all(queries.map(qRunQuery))
+        .done(function(e, r) {
+          callback(e, r);
+        });
+    }
+  };
+  return db;
 };
