e1684e6aca6048e366ab617049f8636dda3333fd
Jim McAndrew <jim@loc8.us>
2014-06-11 18:14:11 -0600

    use request

--- src/download.js
+++ src/download.js
@@ -1,49 +1,52 @@
-var http = require('http'),
+var request = require('request'),
   fs = require('fs'),
   util = require('util');
 
-module.exports = function download(url, dest, callback, encoding, silent) {
+module.exports = function download(url, dest, callback, silent) {
   var file = dest ? fs.createWriteStream(dest) : null,
-    request = http.get(url, function(response) {
-      if (encoding) {
-        response.setEncoding(encoding);
-      }
-      var len = parseInt(response.headers['content-length'], 10),
-        body = '',
-        cur = 0,
-        total = len / 1048576; //1048576 - bytes in  1Megabyte
-
-
-      response.on('data', function(chunk) {
-        if (!file) {
-          body += chunk;
-        }
-        cur += chunk.length;
-        if (!silent) {
-          util.print('Downloading ' + (100.0 * cur / len).toFixed(2) + '% ' + (cur / 1048576).toFixed(2) + ' mb' + '. Total size: ' + total.toFixed(2) + ' mb\r');
-        }
-      });
-
-      response.on('end', function() {
-        if (!silent) {
-          util.print('Download complete                                                 \n\r');
-        }
-        if (!file) {
-          callback(null, body);
-        }
-      });
-
-      request.on('error', function(e) {
-        callback(e);
-        console.log('Error: ' + e.message);
-      });
-
+    totalSize,
+    cur = 0,
+    req = request(url, function(error, resp, body) {
+      if (error) throw error;
       if (file) {
-        response.pipe(file);
         file.on('finish', function() {
           file.close(callback);
         });
+      } else {
+        callback(error, body);
       }
-
     });
+
+  req.on('response', function(response) {
+    totalSize = Number(response.headers['content-length']);
+  });
+
+  req.on('data', function(chunk) {
+    cur += chunk.length;
+    if (!silent) {
+      if (totalSize) {
+        util.print('Downloading ' + (100.0 * cur / totalSize).toFixed(2) + '% ' + (cur / 1048576).toFixed(2) + ' mb' + '. Total size: ' + (totalSize / 1048576).toFixed(2) + ' mb\r');
+      } else {
+        util.print('Downloading ' + (cur / 1048576).toFixed(2) + ' mb\r');
+      }
+    }
+  });
+
+  req.on('end', function() {
+    if (!silent) {
+      util.print('Download complete                                                 \n\r');
+    }
+
+  });
+
+  req.on('error', function(e) {
+    callback(e);
+    console.log('Error: ' + e.message);
+  });
+
+  if (file) {
+    req.pipe(file);
+  }
+
+
 };
