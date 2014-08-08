158db57739d10b674607013ffcf3b384941fe4df
Jim McAndrew <jim@loc8.us>
2014-08-08 16:09:52 -0600

    replace getZoom with getProjectYML

--- src/tasks.js
+++ src/tasks.js
@@ -1,10 +1,13 @@
-var database = require('./database'),
-  configFile = require('../config'),
+var configFile = require('../config'),
+  database = require('./database'),
+  fs = require('fs'),
   tileMath = require('./tilemath'),
   mapboxUpload = require('mapbox-upload'),
   Q = require('q'),
   request = require('request'),
-  tilelive = require('tilelive');
+  tilelive = require('tilelive'),
+  tileMath = require('./tilemath'),
+  yaml = require('js-yaml');
 
 module.exports = {
   database: {
@@ -20,13 +23,11 @@ module.exports = {
     getBounds: function(config, startTime) {
       var deferred = Q.defer();
       var innerTaskList = {
-        getZoom: function() {
+        getProjectYML: function() {
           var innerDeferred = Q.defer();
-          request({
-            url: 'http://a.tiles.mapbox.com/v3/' + config.mbtiles.mapboxId + '.json',
-            json: true
-          }, function(err, res, body) {
-            innerDeferred.resolve(body);
+          fs.readFile(config.tilemill2.projectPath + '/data.yml', function(e,r) {
+            if (e) throw e;
+            innerDeferred.resolve(yaml.load(r));
           });
           return innerDeferred.promise;
         },
@@ -41,13 +42,19 @@ module.exports = {
         }
       };
       Q.all([
-        innerTaskList.getZoom(),
+        innerTaskList.getProjectYML(),
         innerTaskList.getTiles()
       ]).done(function(res) {
         var returnValue = {},
           tiles = [];
         returnValue.minZoom = res[0].minzoom;
         returnValue.maxZoom = res[0].maxzoom;
+        for (var layerIndex = 0; layerIndex < res[0].Layer.length; layerIndex++) {
+          var bufferSize = res[0].Layer[layerIndex].properties['buffer-size'];
+          if (bufferSize && ((returnValue.bufferSize && bufferSize > returnValue.bufferSize) || !returnValue.bufferSize)) {
+            returnValue.bufferSize = bufferSize;
+          }
+        }
         for (var row in res[1][0].result.rows) {
           var currentRow = res[1][0].result.rows[row];
           tiles = tiles.concat(module.exports.tiles.getTilesFromBounds(
@@ -56,7 +63,8 @@ module.exports = {
             parseFloat(currentRow.minlat, 10),
             parseFloat(currentRow.maxlat, 10),
             parseFloat(res[0].minzoom, 10),
-            parseFloat(res[0].maxzoom, 10)
+            parseFloat(res[0].maxzoom, 10),
+            returnValue.bufferSize
           ));
         }
         returnValue.tiles = tiles.filter(function(item, position, base) {
@@ -95,18 +103,18 @@ module.exports = {
     }
   },
   tiles: {
-    getTilesFromBounds: function(minLon, maxLon, minLat, maxLat, minZoom, maxZoom) {
+    getTilesFromBounds: function(minLon, maxLon, minLat, maxLat, minZoom, maxZoom, bufferPx) {
       var tiles = [],
         tms = {};
       for (var zoom = minZoom; zoom <= maxZoom; zoom++) {
         tms[zoom] = {
-          minX: tileMath.long2tile(minLon, zoom),
-          minY: tileMath.lat2tms(minLat, zoom),
-          maxX: tileMath.long2tile(maxLon, zoom),
-          maxY: tileMath.lat2tms(maxLat, zoom)
+          minX: tileMath.long2tile(minLon, zoom, bufferPx * -1),
+          minY: tileMath.lat2tms(minLat, zoom, bufferPx * -1),
+          maxX: tileMath.long2tile(maxLon, zoom, bufferPx),
+          maxY: tileMath.lat2tms(maxLat, zoom, bufferPx)
         };
         for (var xRow = tms[zoom].minX; xRow <= tms[zoom].maxX; xRow++) {
-          for (var yRow = tms[zoom].minY; yRow <= tms[zoom].maxY; yRow++) {
+          for (var yRow = tms[zoom].minY; yRow >= tms[zoom].maxY; yRow--) {
             tiles.push([zoom, xRow, yRow]);
           }
         }
@@ -118,10 +126,10 @@ module.exports = {
     if (
       args.project &&
       args.startTime &&
-      configFile.interfaces[args.project] &&
+      configFile.projects[args.project] &&
       args.startTime) {
       // We will need to check these args at some point
-      callback(configFile.interfaces[args.project], args.startTime);
+      callback(configFile.projects[args.project], args.startTime);
     } else {
       console.log('invalid args (requires -p for project and -d with datetime');
     }
