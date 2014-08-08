e3b62b7fdcdf56a495a7118121f53db9e940afce
Jim McAndrew <jim@loc8.us>
2014-08-08 17:33:16 -0600

    Only download if it's specified in the config

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ var executeTasks = function(config, startTime) {
 
   Q.all([
     // * refresh the rendered data
-    //tasks.renderData(config, startTime),
+    tasks.database.renderData(config, startTime),
     // * Create a list of the required tiles
-    tasks.database.getBounds(config, startTime)
+    tasks.database.getBounds(config, startTime),
     // * Download the last version of mbtiles
-    // tasks.downloadTiles(config.mbtiles.mapboxId)
+    config.mbtiles.downloadFromServer ? tasks.mbtiles.downloadTiles(config.mbtiles.mbtilesDir, config.mbtiles.mapboxId) : null
   ]).done(function(r) {
     // Once the three tasks are done:
     // Copy in the new tiles with tileliveCopy
     // Upload the tiles to mapbox
     console.log('done');
-    console.log(JSON.stringify(r, null, 2));
+    console.log(JSON.stringify(r[1], null, 2));
     process.exit(0);
   });
 };
