529e77ca5205230a1e09925bedafc96fb8f20f00
Jim McAndrew <jim@NPMaps-iMac.local>
2014-08-22 12:10:08 -0600

    exit if there are no changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var argv = require('minimist')(process.argv.slice(2), {
-    alias: {
-      d: 'startTime',
-      date: 'startTime',
-      t: 'startTime',
-      time: 'startTime',
-      p: 'project',
-      i: 'project'
-    }
-  }),
+  alias: {
+    d: 'startTime',
+    date: 'startTime',
+    t: 'startTime',
+    time: 'startTime',
+    p: 'project',
+    i: 'project'
+  }
+}),
   tasks = require('./src/tasks.js'),
   Q = require('q');
 
@@ -23,25 +23,25 @@ var executeTasks = function(config, startTime) {
   ]).done(function(r) {
     // Once the three tasks are done:
     // Copy in the new tiles with tileliveCopy
-    //updateTiles: function(mbtilesFile, tileList, tm2ProjectPath, callback) {¶
-    tasks.mbtiles.updateTiles(r[1], config.mbtiles.mbtilesDir, config.mbtiles.mapboxId, config.tilemill2.projectPath, function(taskResult) {
-      if (taskResult.code === 0) {
-        // Upload the tiles to mapbox
-        if (config.mbtiles.uploadToServer) {
-          console.log('Uploading tiles');
-          tasks.mbtiles.uploadTiles(config.mbtiles.mbtilesDir + '/' + config.mbtiles.mapboxId + '.mbtiles', config.mbtiles.mapboxId, function() {
-            console.log(JSON.stringify(r[1], null, 2));
-            console.log('done');
-            process.exit(0);
-          });
+    if (r[1].tileListFile) {
+      tasks.mbtiles.updateTiles(r[1], config.mbtiles.mbtilesDir, config.mbtiles.mapboxId, config.tilemill2.projectPath, function(taskResult) {
+        if (taskResult.code === 0) {
+          // Upload the tiles to mapbox
+          if (config.mbtiles.uploadToServer) {
+            console.log('Uploading tiles');
+            tasks.mbtiles.uploadTiles(config.mbtiles.mbtilesDir + '/' + config.mbtiles.mapboxId + '.mbtiles', config.mbtiles.mapboxId, function() {
+              tasks.exitProcess('done', r[1], 0);
+            });
+          } else {
+            tasks.exitProcess('', r[1], 0);
+          }
         } else {
-          console.log(JSON.stringify(r[1], null, 2));
-          process.exit(0);
+          tasks.exitProcess('mbtiles update error', r[1], taskResult.code);
         }
-      } else {
-        process.exit(taskResult.code);
-      }
-    });
+      });
+    } else {
+      tasks.exitProcess('No New tiles', r[1], 0);
+    }
   });
 };
 
