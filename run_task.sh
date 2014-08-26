87bafd578b512c24bb1c608e0460b184a63ececb
Jim McAndrew <jim@loc8.us>
2014-08-26 12:35:24 -0600

    Fix some issues with relative paths

--- run_task.sh
+++ run_task.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NEWTIME=`date +"%Y-%m-%d %H:%M:%S"`
-OLDTIME=`tail -n1 .runlog`
+OLDTIME=`tail -n1 $DIR/.runlog`
 if [[ $2 == "all" ]]; then
-  OLDTIME=`head -n1 .runlog`
+  OLDTIME=`head -n1 $DIR/.runlog`
 fi
 
-node ./index.js -p $1 -d "$OLDTIME" && echo $NEWTIME >> .runlog && echo "Complete!"
-
+/usr/local/bin/node $DIR/index.js -p $1 -d "$OLDTIME" && echo $NEWTIME >> $DIR/.runlog && echo "Complete"
